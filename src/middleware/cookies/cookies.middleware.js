baccbd64175b7653a1fba9d2d2fa9d05780ed019
Pharaoh <pharaohmanson@gmail.com>
2024-10-17 11:00:27 -0400

    change

--- src/middleware/cookies/cookies.middleware.js
+++ src/middleware/cookies/cookies.middleware.js
@@ -3,14 +3,13 @@ const cookieSession = require('cookie-session');
 require('dotenv').config();
 
 function setUpCookieMiddleware(){
-    
+    console.log(process.env.COOKIE_KEY)
     return cookieSession(
         {
             name: process.env.COOKIE_NAME,
             maxAge: 7 * 24 * 60 * 60 * 1000,
             keys: [ process.env.COOKIE_KEY ],
-            httpOnly: true,
-            secure: process.env.NODE_ENV === 'production',
+
         }
     );
 };
