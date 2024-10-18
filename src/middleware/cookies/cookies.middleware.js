4c391be8004ed441b90141f988a97baec19a98ef
Pharaoh <pharaohmanson@gmail.com>
2024-10-18 05:52:38 -0400

    cookie settings

--- src/middleware/cookies/cookies.middleware.js
+++ src/middleware/cookies/cookies.middleware.js
@@ -3,13 +3,15 @@ const cookieSession = require('cookie-session');
 require('dotenv').config();
 
 function setUpCookieMiddleware(){
-    console.log(process.env.COOKIE_KEY)
+    
     return cookieSession(
         {
             name: process.env.COOKIE_NAME,
             maxAge: 7 * 24 * 60 * 60 * 1000,
             keys: [ process.env.COOKIE_KEY ],
-
+            secure: process.env.NODE_ENV === 'production',
+            httpOnly: true,
+            sameSite: process.env.NODE_ENV === 'production' ? 'strict' : 'lax',
         }
     );
 };
